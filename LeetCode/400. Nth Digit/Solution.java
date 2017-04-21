8da7b3f024fab9895e3387d06c6d69c8c8398cc8
Nicholas Culmone <NicholasCulmone@gmail.com>
2017-04-20 23:43:10 -0400

    Added solution.

---
+++ LeetCode/400. Nth Digit/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[]args){
+		System.out.println(findNthDigit(15));
+	}
+
+    public static int findNthDigit(int n){
+        int p=0;
+
+        while(n - 9 * Math.pow(10,p) * (p+1) > 0 ){
+        	n -= 9 * Math.pow(10,p) * (p+1);
+        	p++;
+        }
+
+        int dig = p - ((n-1) % (p+1));
+        int num = (int)Math.pow(10,p) + (n-1)/(p+1);
+        int ans=2;
+
+        for(int i=0;i<=dig;i++){
+        	ans = num % 10;
+        	num /= 10;
+        }
+        return ans;
+    }
+}
